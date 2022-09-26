397deead0e905ee7d12fadc768886a7d63d9ef0b
Thxios <ljm0302111@naver.com>
2022-09-26 15:30:12 +0900

    impl main argparse

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
+import argparse
+from simulation import simulation_names, simulations
+from app import Window
+
+
+
+parser = argparse.ArgumentParser(
+    description='Simulations using Pygame',
+)
+parser.add_argument(
+    'SimulationType',
+    type=str,
+    choices=simulation_names,
+    help='simulation type to visualize',
+)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    simulator = simulations[args.SimulationType]
+    Window(simulator()).run()
+
